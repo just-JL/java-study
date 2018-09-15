663237523fa057ac4d81998e2c448eeb401c383f
jiliang <2514935046@qq.com>
2018-09-15 12:05:27 +0800

    demo:jvm\rq\threadpool

---
+++ src/main/java/jvm/TestPermGen.java
@@ -0,0 +1,46 @@
+package jvm;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jiliang
+ * @date 2018/7/29 14:52
+ **/
+public class TestPermGen {
+    private static List<Object> insList = new ArrayList<Object>();
+
+    public static void main(String[] args) throws Exception {
+
+        permLeak();
+    }
+
+    private static void permLeak() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            URL[] urls = getURLS();
+            URLClassLoader urlClassloader = new URLClassLoader(urls, null);
+            Class<?> logfClass = Class.forName("org.apache.commons.logging.LogFactory", true,urlClassloader);
+            Method getLog = logfClass.getMethod("getLog", String.class);
+            Object result = getLog.invoke(logfClass, "TestPermGen");
+            insList.add(result);
+            System.out.println(i + ": " + result);
+        }
+    }
+
+    private static URL[] getURLS() throws MalformedURLException {
+        File libDir = new File("C:/Users/jilia/Desktop");
+        File[] subFiles = libDir.listFiles();
+        int count = subFiles.length;
+        URL[] urls = new URL[count];
+        for (int i = 0; i < count; i++) {
+            urls[i] = subFiles[i].toURI().toURL();
+        }
+        return urls;
+    }
+
+}
---
+++ src/main/java/threadpool/ExecutorServiceDemo.java
@@ -0,0 +1,38 @@
+package threadpool;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+
+import java.util.concurrent.*;
+
+/**
+ * @author jiliang
+ * @date 2018/9/12 20:01
+ **/
+public class ExecutorServiceDemo {
+
+
+    public static void main(String[] args) {
+        // 创建一个线程池对象，控制要创建几个线程对象。
+//        ExecutorService pool = Executors.newFixedThreadPool(2);
+        ThreadFactory namedThreadFactory = new BasicThreadFactory.Builder().namingPattern("example-schedule-pool-%d").daemon(true).build();
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 200,0L, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        // 可以执行Runnable对象或者Callable对象代表的线程
+        pool.submit(new MyRunnable());
+//        pool.submit(new MyRunnable());
+//        pool.execute(new MyRunnable());
+
+        //结束线程池
+        pool.shutdown();
+    }
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int x = 0; x < 10; x++) {
+                System.out.println(Thread.currentThread().getName() + ":" + x);
+            }
+        }
+    }
+}
+
