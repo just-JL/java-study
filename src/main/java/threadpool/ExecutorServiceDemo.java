df6dffdc1233a1ac495b7a9f4fb96e72eb53c70d
jiliang <2514935046@qq.com>
2018-09-17 09:07:48 +0800

    add thread safe

--- src/main/java/threadpool/ExecutorServiceDemo.java
+++ src/main/java/threadpool/ExecutorServiceDemo.java
@@ -10,28 +10,37 @@ import java.util.concurrent.*;
  **/
 public class ExecutorServiceDemo {
 
-
     public static void main(String[] args) {
         // 创建一个线程池对象，控制要创建几个线程对象。
-//        ExecutorService pool = Executors.newFixedThreadPool(2);
         ThreadFactory namedThreadFactory = new BasicThreadFactory.Builder().namingPattern("example-schedule-pool-%d").daemon(true).build();
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 200,0L, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 5,0L, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        NotThreadSafe notThreadSafe = new NotThreadSafe();
+        NotThreadSafe notThreadSafe1 = new NotThreadSafe();
 
         // 可以执行Runnable对象或者Callable对象代表的线程
-        pool.submit(new MyRunnable());
-//        pool.submit(new MyRunnable());
+        pool.submit(new MyRunnable(notThreadSafe));
+        pool.submit(new MyRunnable(notThreadSafe1));
 //        pool.execute(new MyRunnable());
 
         //结束线程池
         pool.shutdown();
     }
+
     static class MyRunnable implements Runnable {
+        NotThreadSafe notThreadSafe = null;
+
+        public MyRunnable(NotThreadSafe notThreadSafe) {
+            this.notThreadSafe = notThreadSafe;
+        }
 
         @Override
         public void run() {
-            for (int x = 0; x < 10; x++) {
-                System.out.println(Thread.currentThread().getName() + ":" + x);
-            }
+//            for (int x = 0; x < 10; x++) {
+//                System.out.println(Thread.currentThread().getName() + ":" + x);
+//            }
+            notThreadSafe.add(Thread.currentThread().getName() + "_");
+            System.out.println(notThreadSafe.toString());
         }
     }
 }
---
+++ src/main/java/threadpool/NotThreadSafe.java
@@ -0,0 +1,20 @@
+package threadpool;
+
+/**
+ * @author jiliang
+ * @date 2018/9/15 17:58
+ **/
+public class NotThreadSafe {
+    StringBuilder builder = new StringBuilder();
+
+    public void add(String text){
+        this.builder.append(text);
+    }
+
+    @Override
+    public String toString() {
+        return "NotThreadSafe{" +
+                "builder=" + builder +
+                '}';
+    }
+}
